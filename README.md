784d562544cf5fb46f0c69016452d39703827768
Abdurrahan Saleh <abi_abdurrahman@hotmail.com>
2019-03-02 17:46:03 +0700

    Pembuatan class titik

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # latihan-propen
+
+# update 1 (02/03/2019) tambah class titik
---
+++ python.py
@@ -0,0 +1,7 @@
+class titik:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+suatuTitik = titik(2,4)
+print(suatuTitik.x)
